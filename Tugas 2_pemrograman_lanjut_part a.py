7b7b0960b19aeebb272897e5218d58cdebf32870
RifqyAzhar <167677412+RifqyAzhar@users.noreply.github.com>
2024-05-15 20:08:08 +0700

    Add files via upload

---
+++ Tugas 2_pemrograman_lanjut_part a.py
@@ -0,0 +1,12 @@
+'''
+NAMA   : Muhammad Rifqy Azhar Widiyatmoko
+NIM    : 41823010123
+MATKUL : Pemrograman Lanjut 2
+'''
+
+'''
+PART A
+'''
+angka = int(input("MASUKAN ANGKA (1-20) : "))
+for i in range(angka):
+    print(i*i)
---
+++ Tugas 2_pemrograman_lanjut_part b.py
@@ -0,0 +1,13 @@
+'''
+NAMA   : Muhammad Rifqy Azhar Widiyatmoko
+NIM    : 41823010123
+MATKUL : Pemrograman Lanjut 2
+'''
+
+'''
+PART B
+'''
+angka = int(input("MASUKAN ANGKA (1-20) : "))
+for i in range(angka):
+    if i % 2 == 1:
+        print(i*i)
